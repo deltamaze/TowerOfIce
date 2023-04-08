eb0cb10fefb70727a4343f3011a8753577d5ccc8
deltamaze <wcpooley@gmail.com>
2023-04-07 20:34:47 -0500

    Support linting for typescript

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import * as PIXI from 'pixi.js';
+import { moveBall } from './moveBall';
+
+const app = new PIXI.Application<HTMLCanvasElement>({
+  width: 400,
+  height: 800,
+  backgroundColor: 0xffffff
+});
+
+// black border
+app.renderer.view.style.border = '1px solid black';
+
+// add floor rectangle
+export const floor = new PIXI.Graphics();
+floor.beginFill(0x000000);
+floor.drawRect(0, 0, 400, 10);
+floor.endFill();
+floor.y = 700;
+app.stage.addChild(floor);
+
+// add ball
+export const ball = new PIXI.Graphics();
+ball.beginFill(0x000000);
+ball.drawCircle(0, 0, 10);
+ball.endFill();
+ball.x = 200;
+ball.y = 200;
+app.stage.addChild(ball);
+
+// process frame
+const processFrame = () => {
+  moveBall(floor);
+  requestAnimationFrame(processFrame);
+};
+
+// start frame
+requestAnimationFrame(processFrame);
+
+document.body.appendChild(app.view);
--- src/moveBall.ts
+++ src/moveBall.ts
@@ -1,64 +1,30 @@
-import * as PIXI from 'pixi.js';
-
-const app = new PIXI.Application({
-  width: 400,
-  height: 800,
-});
-
-// white background
-app.renderer.backgroundColor = 0xffffff; 
-// black border
-app.renderer.view.style.border = '1px solid black';
-
-// add floor rectangle
-const floor = new PIXI.Graphics();
-floor.beginFill(0x000000);
-floor.drawRect(0, 0, 400, 10);
-floor.endFill();
-floor.y = 700;
-app.stage.addChild(floor);
-
-// add ball
-const ball = new PIXI.Graphics();
-ball.beginFill(0x000000);
-ball.drawCircle(0, 0, 10);
-ball.endFill();
-ball.x = 200;
-ball.y = 200;
-app.stage.addChild(ball);
+import { ball, floor } from '.';
 
 // add ball velocity
 let ballVelocity = {
   x: 0,
   y: 0,
 };
-
 // add ball acceleration
 let ballAcceleration = {
   x: 0,
   y: 0,
 };
-
 // add ball force
 let ballForce = {
   x: 0,
   y: 0,
 };
-
 // add ball mass
 const ballMass = 1;
-
 // add ball friction
 const ballFriction = 0.99;
-
 // add ball gravity
 const ballGravity = 0.1;
-
 // add ball bounce
 const ballBounce = 0.9;
-
 // move ball
-const moveBall = () => {
+export const moveBall = (nearestPlatform) => {
   // add ball acceleration
   ballAcceleration.x = ballForce.x / ballMass;
   ballAcceleration.y = ballForce.y / ballMass;
@@ -73,14 +39,14 @@ const moveBall = () => {
 
   // add ball gravity
   ballVelocity.y += ballGravity;
-  
+
   // add ball position
   ball.x += ballVelocity.x;
   ball.y += ballVelocity.y;
 
   // add ball bounce
-  if (ball.y + ball.height > floor.y) {
-    ball.y = floor.y - ball.height;
+  if (ball.y + ball.height > nearestPlatform.y) {
+    ball.y = nearestPlatform.y - ball.height;
     ballVelocity.y *= -ballBounce;
   }
 
@@ -88,15 +54,3 @@ const moveBall = () => {
   ballForce.x = 0;
   ballForce.y = 0;
 };
-
-// process frame
-const processFrame = () => {
-  moveBall();
-  requestAnimationFrame(processFrame);
-};
-
-// start frame
-requestAnimationFrame(processFrame);
-
-
-document.body.appendChild(app.view);
